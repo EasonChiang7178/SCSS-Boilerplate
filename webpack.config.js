66d5107b3be58df9d697a06e17fa8448a2833e6e
EasonChiange7178 <ishiu.chiang@gmail.com>
2016-04-16 23:24:47 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: './assets/js/app.js',
+  },
+
+  output: {
+    path: './build',
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      /* ----- JavaScript Loader ----- */
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+
+      /* ----- SCSS Loader ----- */
+      {
+        test: /\.(scss|sass)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: ExtractTextPlugin.extract('style-loader',
+                  'css-loader!sass-loader?includePaths[]=' +
+                  path.resolve(__dirname, './node_modules/compass-mixins/lib')),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('./[name].css'),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss'],
+  }
+};
